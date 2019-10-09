ffad0744adf1c7b5425accd219ef7570fbbb612b
George Zhou <georgezhou89@hotmail.com>
2019-10-09 10:04:36 -0400

    Added Employee Class

--- src/LabAssignment3/Employee.java
+++ src/LabAssignment3/Employee.java
@@ -1,4 +1,37 @@
 package LabAssignment3;
 
 public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(){
+
+    }
+
+    public Employee(String name, double salary){
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public void setSalary(double salary){
+        this.salary=salary;
+    }
+
+    public String toString(){
+        String result = "Name: " + this.getName()+"\n";
+        result += "Yearly Salary: " + this.getSalary() + "\n";
+        return result;
+    }
 }
